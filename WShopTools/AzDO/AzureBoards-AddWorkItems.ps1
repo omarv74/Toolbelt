e38f0c35544a33e11bfebf7a9873af7ed24aa27a
D. Omar Villarreal <davillar@microsoft.com>
2020-07-06 22:00:21 -0500

    Updated AddWorkItems

--- WShopTools/AzDO/AzureBoards-AddWorkItems.ps1
+++ WShopTools/AzDO/AzureBoards-AddWorkItems.ps1
@@ -11,22 +11,5 @@ $rootIterationPath = "\$projectName\Iteration\Ecolab"
 
 az devops configure --defaults organization=$orgURL project=$projectName
 
-az boards area project create --name 'Legacy Portfolio' --path "$rootAreaPath"
-az boards area project create --name 'Modern Portfolio' --path "$rootAreaPath"
-az boards area project create --name 'CRM Program' --path "$rootAreaPath\Modern Portfolio"
-az boards area project create --name 'ERP Program' --path "$rootAreaPath\Modern Portfolio"
-az boards area project create --name 'Mobile CRM' --path "$rootAreaPath\Modern Portfolio\CRM Program"
-az boards area project create --name 'Web CRM' --path "$rootAreaPath\Modern Portfolio\CRM Program"
+az boards work-item create  --type Epic  --title 'Identify Change Requirements' --area 'DemoWaterfall\Ecolab\Reporting'
 
-az boards iteration project create --name 'Fiscal Year - H1' --path "$rootIterationPath"
-az boards iteration project create --name 'Fiscal Year - H2' --path "$rootIterationPath"
-
-az boards iteration project create --name 'PI 1' --path "$rootIterationPath\Fiscal Year - H1" --start-date (Get-Date $sprint1StartDate -Format 'yyy-MM-dd') --finish-date (Get-Date $sprint1StartDate.AddDays(4+(2*7)) -Format 'yyy-MM-dd')
-az boards iteration project create --name 'Sprint 1' --path "$rootIterationPath\Fiscal Year - H1\PI 1" --start-date (Get-Date $sprint1StartDate -Format 'yyy-MM-dd') --finish-date (Get-Date $sprint1StartDate.AddDays(4+(0*7)) -Format 'yyy-MM-dd')
-az boards iteration project create --name 'Sprint 2' --path "$rootIterationPath\Fiscal Year - H1\PI 1" --start-date (Get-Date $sprint1StartDate.AddDays(1*7) -Format 'yyy-MM-dd') --finish-date (Get-Date $sprint1StartDate.AddDays(4+(1*7)) -Format 'yyy-MM-dd')
-az boards iteration project create --name 'Sprint 3' --path "$rootIterationPath\Fiscal Year - H1\PI 1" --start-date (Get-Date $sprint1StartDate.AddDays(2*7) -Format 'yyy-MM-dd') --finish-date (Get-Date $sprint1StartDate.AddDays(4+(2*7)) -Format 'yyy-MM-dd')
-
-az boards iteration project create --name 'PI 2' --path "$rootIterationPath\Fiscal Year - H1" --start-date (Get-Date $sprint1StartDate.AddDays(3*7) -Format 'yyy-MM-dd') --finish-date (Get-Date $sprint1StartDate.AddDays(4+(3*7)) -Format 'yyy-MM-dd')
-az boards iteration project create --name 'Sprint 4' --path "$rootIterationPath\Fiscal Year - H1\PI 2" --start-date (Get-Date $sprint1StartDate.AddDays(3*7) -Format 'yyy-MM-dd') --finish-date (Get-Date $sprint1StartDate.AddDays(4+(3*7)) -Format 'yyy-MM-dd')
-az boards iteration project create --name 'Sprint 5' --path "$rootIterationPath\Fiscal Year - H1\PI 2" --start-date (Get-Date $sprint1StartDate.AddDays(4*7) -Format 'yyy-MM-dd') --finish-date (Get-Date $sprint1StartDate.AddDays(4+(4*7)) -Format 'yyy-MM-dd')
-az boards iteration project create --name 'Sprint 6' --path "$rootIterationPath\Fiscal Year - H1\PI 2" --start-date (Get-Date $sprint1StartDate.AddDays(5*7) -Format 'yyy-MM-dd') --finish-date (Get-Date $sprint1StartDate.AddDays(4+(5*7)) -Format 'yyy-MM-dd')
